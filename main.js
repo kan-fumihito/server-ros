ca00831d1a595ead8cb81e72bfe672c1bec4eb36
kan-fumihito <kan_f1204@docomo.ne.jp>
2021-06-19 23:04:57 +0900

    交替処理追加

--- main.js
+++ main.js
@@ -2,20 +2,31 @@ const WebSocket = require("ws");
 
 // ブラウザから受け取るだけ
 const wsUser = new WebSocket.Server({ port: 8080 });
+var wsList = [];
+var wsNow = null;
+var wsIdx = 0;
 // Rosに送るだけ
 const wsRos = new WebSocket.Server({ port: 9090 });
 
 wsUser.on("connection", function(ws) {
     console.log("Open User");
+    wsList.push(ws);
+    if (wsNow == null) {
+        wsNow = ws;
+        ws.send("You");
+    }
     ws.on("message", function(msg) {
         console.log("Received: " + msg);
-        wsRos.clients.forEach(function(client) {
+        if (ws == wsNow) {
             console.log("Send to Ros");
-            client.send(msg);
-        });
+            wsRos.clients.forEach(function(client) {
+                client.send(msg);
+            });
+        }
     });
     ws.on("close", function() {
         console.log("Close User");
+        wsList.pop(ws);
     });
 });
 
@@ -24,4 +35,16 @@ wsRos.on("connection", function(ws) {
     ws.on("close", function() {
         console.log("Close ROS");
     });
+    ws.on("message", function(msg) {
+        console.log(msg);
+    })
 });
+
+function changeNow() {
+    if (wsList.length > 0) {
+        wsIdx = (wsIdx + 1) % wsList.length;
+        wsNow = wsList.indexOf(wsIdx);
+        wsNow.send("You");
+    }
+}
+setInterval(changeNow, 1000 * 10);
